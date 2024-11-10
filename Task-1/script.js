fe77e15e2b118623050fea6cc9fc52ac37652c16
Tanmay Ramteke <tanmay.github.21@gmail.com>
2024-11-11 00:37:44 +0530

    Done all Tasks

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,27 +1,31 @@
 let screen = document.getElementById('screen');
 let currentInput = '';
 
+// Append number to current input
 function appendNumber(number) {
   currentInput += number;
   screen.value = currentInput;
 }
 
+// Append operator to current input
 function appendOperator(operator) {
- if (currentInput === '' || /[+\-*/]$/.test(currentInput)) return;
+  if (currentInput === '' || /[+\-*/]$/.test(currentInput)) return;
   currentInput += operator;
   screen.value = currentInput;
 }
 
+// Clear screen
 function clearScreen() {
   currentInput = '';
   screen.value = '';
 }
 
+// Evaluate the expression and display result
 function calculate() {
   try {
-    currentInput = eval(currentInput).toString(); // evaluete the expression
+    currentInput = eval(currentInput).toString();  // Evaluate the expression
     screen.value = currentInput;
   } catch (error) {
-    screen.value = 'error';
+    screen.value = 'Error';
   }
 }
